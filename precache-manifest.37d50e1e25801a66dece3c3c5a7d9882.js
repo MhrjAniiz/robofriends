883a68efe3dab0885035e02cc4ca6c865bec51ee
MhrjAniiz <anishmaharjan17@gmail.com>
2020-05-28 13:46:21 +0545

    Updates

---
+++ precache-manifest.37d50e1e25801a66dece3c3c5a7d9882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b21a62dda2deab915ab0970914eaf98",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "46ccfee27ae1a93dec97",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "58ac4ecdd5af83d9e2e3",
+    "url": "/robofriends/static/css/main.453dd51e.chunk.css"
+  },
+  {
+    "revision": "46ccfee27ae1a93dec97",
+    "url": "/robofriends/static/js/2.3ad53ea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.3ad53ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ac4ecdd5af83d9e2e3",
+    "url": "/robofriends/static/js/main.403d6264.chunk.js"
+  },
+  {
+    "revision": "43f8e3a834b0d2a6d57b",
+    "url": "/robofriends/static/js/runtime-main.c1549296.js"
+  }
+]);
